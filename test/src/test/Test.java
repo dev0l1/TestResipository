8e15d05614d75a0bce3dcd29dc3669d95808c09b
TyRo <tyrobin@web.de>
2021-05-11 14:33:03 +0200

    - added for loop

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -3,7 +3,11 @@ package test;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Hello World");
+		}
+		
 
 	}
 
